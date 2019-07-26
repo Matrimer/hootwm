af526de3b7b8bf4bd346f1693b6c48b609bc0367
Matrimer <44270049+Matrimer@users.noreply.github.com>
2019-07-27 00:35:04 +0200

    Changed desktops to 9 and lowered master size

--- config.h
+++ config.h
@@ -1,6 +1,6 @@
-#define MASTER_SIZE 0.6
+#define MASTER_SIZE 0.55
 #define BORDER 3
 #define GAP 1
 #define PIPE_FILE "/tmp/lswm"
 #define MIN 10 // minimum window size
-#define DESKTOPS 3
+#define DESKTOPS 9
